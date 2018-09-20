294e1383a7d544f6d8627e51bae41187e5b3aa39
CheSangPark <38177490+CheSangPark@users.noreply.github.com>
2018-09-20 11:38:33 +0900

    Create EX3.ml

---
+++ HW2/EX3.ml
@@ -0,0 +1,4 @@
+let heap1 = NODE (1, 1, NODE (0, 5, EMPTY, EMPTY), NODE (0, 3, EMPTY, EMPTY)) in 
+let heap2 = NODE (0, 2, NODE (0, 4, EMPTY, EMPTY), EMPTY) in 
+findMin(merge (heap1, heap2));;
+(*- : value = 1*)

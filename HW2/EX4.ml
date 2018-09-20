f178e1bd586c9b9b983ea6829775681aace1f2e2
jinsun1117 <38969914+jinsun1117@users.noreply.github.com>
2018-09-20 11:51:56 +0900

    Update EX4.ml

--- HW2/EX4.ml
+++ HW2/EX4.ml
@@ -3,3 +3,4 @@ let (n, queue') = IntListQ.deQ (IntListQ.enQ(IntListQ.enQ(IntListQ.emptyQ, [1;2;
 val n : int list = [1; 2; 3] 
 val queue' : int list list * int list list = ([], [[4; 5]])
 *)
+(*testasdf*)

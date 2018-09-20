f6af81bbfcab4cb38b49daa8912f37adee8238b8
CheSangPark <38177490+CheSangPark@users.noreply.github.com>
2018-09-20 11:39:30 +0900

    Update EX1.ml

--- HW2/EX1.ml
+++ HW2/EX1.ml
@@ -1,2 +1,5 @@
- calculate(SIGMA(INT 1, ADD(X,X), X));;
+calculate(SIGMA(INT 1, ADD(X,X), X));;
 (*Exception: FreeVariable.*)
+
+calculate(INTEGRAL(REAL 1.0, REAL (-5.0), X));;
+(*- : float = 12.3000000000000114*)

20eb5c0a2b35a1a771bc6d0a54d987e52d648913
CheSangPark <38177490+CheSangPark@users.noreply.github.com>
2018-09-20 12:10:16 +0900

    Update EX3.ml

--- HW2/EX3.ml
+++ HW2/EX3.ml
@@ -2,3 +2,8 @@ let heap1 = NODE (1, 1, NODE (0, 5, EMPTY, EMPTY), NODE (0, 3, EMPTY, EMPTY)) in
 let heap2 = NODE (0, 2, NODE (0, 4, EMPTY, EMPTY), EMPTY) in 
 findMin(merge (heap1, heap2));;
 (*- : value = 1*)
+
+let heap1 = NODE(0, 2, EMPTY, EMPTY) in
+let heap2 = NODE(0, 3, EMPTY, EMPTY) in
+findMin(merge (heap1, heap2));;
+(*- : value = 2*)

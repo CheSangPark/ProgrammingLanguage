c40beeaaa57be4049eb653067aa0f7cbe0ad30ab
CheSangPark <38177490+CheSangPark@users.noreply.github.com>
2018-09-20 12:07:23 +0900

    Update EX2.ml

--- HW2/EX2.ml
+++ HW2/EX2.ml
@@ -1,3 +1,3 @@
 let exp =SUM ([TIMES [CONST 5; TIMES([VAR "x";VAR "x"])]; CONST 1]) in 
  diff(exp, "x");;
- (**)
+ (* - : ae =SUM [TIMES [CONST 5; SUM [TIMES [CONST 1; TIMES [VAR "x"]]; TIMES [VAR "x"; CONST 1]]]; CONST 0] *)
